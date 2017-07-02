efb9a61daf0a390917a405a390b306d8f7ecbf00
jmgf <joaomgfernandes@hotmail.com>
2017-07-02 17:32:49 +0100

    Adds some forms

--- Taskager_joao/src/java/web/ProjectServlet.java
+++ Taskager_joao/src/java/web/ProjectServlet.java
@@ -55,12 +55,17 @@ public class ProjectServlet extends HttpServlet {
         //String[] tasks = {"1. Pintar a sala", "2. Construir a garagem", "3. Viver a vida","4. Outra tarefa","5. Mais uma","6. Elas continuam", "7. Ainda outra", "8. Tarefa final"};
         String[] tasks = {"Pintar a sala", "Construir a garagem", "Viver a vida","Outra tarefa","Mais uma","Elas continuam", "Ainda outra", "Tarefa final"};        
         String[] subTasks = {"Saltar para a esquerda", "Rodopiar duas vezes", "Subir uma escada", "Puxar uma corda", "Descer uma rampa", "Cair num poço", "Descobrir o tesouro", "Morrer preso"};        
-        
+        //String[] subTasks ={};
+        //String[] tasks ={};
         String[] members = {"Gabriel", "João", "Joel", "Nelson"};        
         
         request.setAttribute("tasks",tasks);
         request.setAttribute("subTasks",subTasks);
         request.setAttribute("members",members);
+        
+        if (request.getParameter("project_status")!=null)
+            request.setAttribute("project_status",request.getParameter("project_status"));
+        else request.setAttribute("project_status","Em progresso");
         
         request.getRequestDispatcher("WEB-INF/ProjectAux.jsp").forward(request, response);
     }
